7614a9aadd3cca706b2763546efacfbaf0e5f6bb
Jeremy Price <jfprice@iupui.edu>
2022-08-07 14:32:22 -0400

    :memo: Added documenttation to code
    #

--- mtf-process-clusters.R
+++ mtf-process-clusters.R
@@ -71,6 +71,10 @@ binary.numeric.translate <- function(the.frame, start.col) {
   return(the.frame)
 }
 
+# Define colors, using IU branding colors https://www.iu.edu/brand/brand-expression/visual-language/color/index.html
+iu.colors <- c("#990000", "#FFAA00", "#056E41", "#006298", "#59264D")
+
+
 ###########################################
 #                 Load Data               #
 ###########################################
@@ -127,9 +131,9 @@ keyword2.frame <- keyword2.frame %>% filter(name != "") %>%
 descriptors.frame <- safe_full_join(keyword1.frame, keyword2.frame, by = "manuscriptID", conflict = coalesce) %>%
   replace(is.na(.), 0)
 
-####                                             ####
+#####################################################
 # Identify descriptors to exclude to reduce "noise" #
-####                                             ####
+#####################################################
 
 # The first list to exclude are those that deal with research methods since
 # we human-coded these in a way that makes more sense to the analysis
@@ -156,6 +160,7 @@ exclude.list <- c("college_students", "education_courses", "graduate_students",
                   "preservice_teacher_education", "educational_technology", "technology_education",
                   "preservice_teachers")
 
+# Exclude extraneous descriptors that address programmatic and course items
 program.metadata.list <- c("program_descriptions", "program_development", "teacher_education_programs",
                            "summer_programs", "program_effectiveness", "program_implementation",
                            "program_design")
@@ -163,28 +168,40 @@ program.metadata.list <- c("program_descriptions", "program_development", "teach
 course.metadata.list <- c("course_content", "course_descriptions", "course_objectives",
                           "course_organization", "objectives", "workshops", "independent_study")
 
+# Actually remove the descriptors here...
 descriptors.frame <- descriptors.frame %>% select(-all_of(exclude.list)) %>%
   select(-all_of(methods.list)) %>%
   select(-all_of(program.metadata.list)) %>%
   select((-all_of(course.metadata.list)))
 
+# ...And order them alphabetically
 descriptors.frame <- descriptors.frame %>% select(order(colnames(descriptors.frame)))
 
+#####################################################
+#         Inital Combination of Descriptors         #
+#####################################################
 
+# This reduction of descriptors involves combining original descriptors into categories.
+# The categories are summed so that a sense of scale is retained. This also allows
+# them to be continuous variables for PCA analysis. Even though some descriptors can
+# be matched to specific fields and efforts in education, the definitions are considered
+# broad rather than narrow.
 
-
+# Combine descriptors having to do with attitudes and beliefs
 attitudes.beliefs.list <- c("attitudes", "beliefs", "teacher_attitudes", "student_attitudes",
                             "administrator_attitudes", "student_teacher_attitudes")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(attitudes_beliefs = sum(c_across(any_of(attitudes.beliefs.list)))) %>%
   select(-all_of(attitudes.beliefs.list))
 
+# Combine descriptors having to do with educational change efforts
 educational.change.list <- c("educational_innovation", "educational_trends", "experimental_programs",
                              "instructional_innovation", "intervention", "transformational_leadership")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(education_change = sum(c_across(any_of(educational.change.list)))) %>%
   select(-all_of(educational.change.list))
 
+# Combine descriptors having to do with English-Language Arts
 ela.list <- c("childrens_literature", "comprehension", "content_area_reading",
               "content_area_writing", "english_instruction", "language_arts", "literacy",
               "literacy_education", "poetry", "reader_response", "reading_assignments",
@@ -196,6 +213,7 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(ela = sum(c_across(any_of(ela.list)))) %>%
   select(-all_of(ela.list))
 
+# Combine descriptors having to do with field experiences specifically
 field.experiences.list <- c("college_school_cooperation", "field_experience_programs",
                             "internship_programs", "partnerships_in_education",
                             "professional_development_schools", "student_teaching", "practicums",
@@ -204,6 +222,7 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(field_experiences = sum(c_across(any_of(field.experiences.list)))) %>%
   select(-all_of(field.experiences.list))
 
+# Combine descriptors having to do with Instructional Design
 instructional.design.list <- c("assignments", "curriculum_development", "curriculum_implementation",
                                "educational_objectives", "educational_planning",
                                "instructional_material_evaluation", "learning_activities",
@@ -214,12 +233,14 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(instructional_design = sum(c_across(any_of(instructional.design.list)))) %>%
   select(-all_of(instructional.design.list))
 
+# Combine descriptors having to do with learing systems, online learning, etc.
 learning.systems.list <- c("computer_assisted_instruction", "courseware", "integrated_learning_systems",
                            "web_based_instruction", "online_courses", "electronic_learning")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(learning_systems = sum(c_across(any_of(learning.systems.list)))) %>%
   select(-all_of(learning.systems.list))
 
+# Combine descriptors having to do with metacognition and psychology
 metacognition.list <- c("cognitive_development", "decision_making", "prior_learning",
                         "problem_solving", "thinking_skills", "transfer_of_training", "value_judgment",
                         "self_evaluation_individuals", "self_evaluation_groups",
@@ -228,11 +249,13 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(metacognition = sum(c_across(any_of(metacognition.list)))) %>%
   select(-all_of(metacognition.list))
 
+# Combine descriptors having to do with simulations and games
 simulations.games.list <- c("computer_simulation", "games", "simulated_environment")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(simulations_games = sum(c_across(any_of(simulations.games.list)))) %>%
   select(-all_of(simulations.games.list))
 
+# Combine descriptors having to do with specific technologies and how they are being used/evaluated
 specific.technologies.list <-c ("audio_equipment", "computer_mediated_communication",
                                 "computer_software", "electronic_equipment", "electronic_publishing",
                                 "handheld_devices", "internet", "laptop_computers",
@@ -244,12 +267,14 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(specific_technologies = sum(c_across(any_of(specific.technologies.list)))) %>%
   select(-all_of(specific.technologies.list))
 
+# Combine descriptors having to do with special education
 sped.list <- c("special_education_teachers", "inclusion", "individualized_instruction",
                "disabilities")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(sped = sum(c_across(any_of(sped.list)))) %>%
   select(-all_of(sped.list))
 
+# Combine descriptors having to do with science, technology, engineering, and mathematics education
 stem.education.list <- c("astronomy", "elementary_school_science", "inquiry", "investigations",
                          "science_activities", "science_education", "secondary_school_science",
                          "science_teachers", "science_instruction", "scientific_methodology",
@@ -260,17 +285,20 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(stem_education_sum = sum(c_across(any_of(stem.education.list)))) %>%
   select(-all_of(stem.education.list)) %>% dplyr::rename(stem_education = stem_education_sum)
 
+# Combine descriptors having to do with issues that are structural in nature
 structural.technology.issues.list <- c("access_to_computers", "barriers", "financial_support",
                                        "troubleshooting")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(structural_technology_issues = sum(c_across(any_of(structural.technology.issues.list)))) %>%
   select(-all_of(structural.technology.issues.list))
 
+# Combine descriptors having to do with teacher educators, not just teacher education
 teacher.educators.list <- c("college_faculty", "faculty", "faculty_development")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(teacher_educators = sum(c_across(any_of(teacher.educators.list)))) %>%
   select(-all_of(teacher.educators.list))
 
+# Combine descriptors having to do with teacher effectiveness
 teacher.effectiveness.list <- c("achievement_gains", "alignment_education", "competence",
                                 "competency_based_teacher_education", "expertise",
                                 "instructional_effectiveness", "performance_factors",
@@ -281,6 +309,7 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(teacher_effectiveness = sum(c_across(any_of(teacher.effectiveness.list)))) %>%
   select(-all_of(teacher.effectiveness.list))
 
+# Combine descriptors having to do with technology literacy
 technology.literacy.list <- c("computer_literacy", "information_literacy", "information_skills",
                               "media_literacy", "multiple_literacies", "technological_literacy",
                               "technology_transfer")
@@ -288,13 +317,19 @@ descriptors.frame <- descriptors.frame %>% rowwise() %>%
   dplyr::mutate(technology_literacy = sum(c_across(any_of(technology.literacy.list)))) %>%
   select(-all_of(technology.literacy.list))
 
+# Combine descriptors having to do with TPACK, a well-known framework in the field
 tpack.list <- c("tpack", "knowledge", "pedagogical_content_knowledge", "knowledge_level")
 descriptors.frame <- descriptors.frame %>% rowwise() %>% 
   dplyr::mutate(tpack_sum = sum(c_across(any_of(tpack.list)))) %>%
   select(-all_of(tpack.list)) %>% dplyr::rename(tpack = tpack_sum)
 
+# Reduce descriptors by removing any that apply to two or fewer articles.
+# There is a fine line between enough data and too much data, and this step is
+# done to set the balance to enough data without losing a sense of how the
+# articles connect with one another.
 descriptors.frame <- descriptors.frame[colSums(descriptors.frame >= 2) > 0]
 descriptors.frame <- as.data.frame(descriptors.frame)
+# Add "descriptors_" tag to column names to identify them as descriptors
 descriptors.frame <- descriptors.frame %>% setNames(paste0('descriptors_', names(.))) %>%
   dplyr::rename("manuscriptID" = "descriptors_manuscriptID")
 
@@ -327,26 +362,42 @@ variables.jaccard <- scale(vegdist(t(analysis.frame), method = "jaccard"))
 #       Run Initial PCA Analysis          #
 ###########################################
 
-#pdf("initial_pca-plot.pdf", width = 36, height = 36)
-#  analysis.pca <- PCA(analysis.frame)
-#dev.off()
+# An initial Primary Component Analysis is conducted to identify overlapping
+# factors for further reduction based on the visualization.
 
-# condense into scales
+initial.analysis.pca <- PCA(analysis.frame, graph = FALSE)
+ggsave("output/initial-pca.pdf", width = 8.5, height = 8.5, units = "in", dpi = 300)
+
+###########################################
+#    Further Condense Data Into Scales    #
+###########################################
+# Based on examination of the PCA visualization, factors from across the full
+# dataframe are reduced by combining them into scales. Like the process with the
+# descriptors only, these are summed to provide a sense of scale and therefore
+# represent continuous variables rather than categorical ones.
+
+# Prepare the dataframe for scaling by first making a copy
 analysis.scales <- analysis.frame
+# A manuscriptID key column is created because column names are removed during
+# dplyr mutate procedures and then moved to the first position.
 analysis.scales$manuscriptID <- rownames(analysis.scales)
 rownames(analysis.scales) <- NULL
 analysis.scales <- analysis.scales %>% relocate(manuscriptID)
+
+# Combine factors that connect with socialization into the teaching field
 social.scale.list <- c("traditions_deliberative", "genres_interpretive", "purposes_socialization")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_social = sum(c_across(any_of(social.scale.list)))) %>%
   select(-all_of(social.scale.list))
 
+# Combine factors that connect with curriculum and academics
 scale.curriculum.list <- c("purposes_academic", "traditions_academic", "genres_other",
                       "genres_design", "sources_review")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_curriculum = sum(c_across(any_of(scale.curriculum.list)))) %>%
   select(-all_of(scale.curriculum.list))
 
+# Combine factors that connect with social justice
 scale.justice.list <- c("purposes_justice", "purposes_other_purpose", 
                         "traditions_reconstruction", "traditions_other_tradition")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
@@ -354,6 +405,7 @@ analysis.scales <- analysis.scales %>% rowwise() %>%
   select(-all_of(scale.justice.list))
 analysis.scales <- analysis.scales %>% select(-scale_justice)
 
+# Combine factors that connect with the "Teacher Preparation Pipeline"
 scale.pipeline.list <- c("traditions_technical", "genres_effects", "descriptors_tpack",
                          "sources_survey", "purposes_capital", "descriptors_teacher_effectiveness",
                          "descriptors_metacognition", "traditions_developmentalist")
@@ -361,57 +413,63 @@ analysis.scales <- analysis.scales %>% rowwise() %>%
   dplyr::mutate(scale_pipeline = sum(c_across(any_of(scale.pipeline.list)))) %>%
   select(-all_of(scale.pipeline.list))
 
+# Combine factors that connect with methods courses
 scale.methods.list <- c("sources_observation", "descriptors_teacher_educators",
                         "sources_artifact")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_methods = sum(c_across(any_of(scale.methods.list)))) %>%
   select(-all_of(scale.methods.list))
 
+# Combine factors that connect with understanding with a phenomological perspective
 scale.phenomenology.list <- c("genres_practitioner", "sources_interview", "purposes_subjectification")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_phenomenology = sum(c_across(any_of(scale.phenomenology.list)))) %>%
   select(-all_of(scale.phenomenology.list))
 
+# Combine factors that connect with knowing how to use technology in authentic experiences
 scale.tech.in.field.list <- c("descriptors_field_experiences", "descriptors_technology_literacy")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_tech_in_field = sum(c_across(any_of(scale.tech.in.field.list)))) %>%
   select(-all_of(scale.tech.in.field.list))
 
-#scale.sol.list <- c("descriptors_instructional_design", "descriptors_stem_education",
-#                    "descriptors_sped", "scale_curriculum")
-#analysis.scales <- analysis.scales %>% rowwise() %>% 
-#  dplyr::mutate(scale_science_of_teaching_learning = sum(c_across(any_of(scale.sol.list)))) %>%
-#  select(-all_of(scale.sol.list))
-
+# Combine factors that connect with specific academic subjects
 scale.subjects.list <- c("descriptors_stem_education", "descriptors_sped", "scale_curriculum",
                     "descriptors_ela")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_subjects = sum(c_across(any_of(scale.subjects.list)))) %>%
   select(-all_of(scale.subjects.list))
 
+# Combine factors that connect with socializing into authentic technology use
 scale.social.tech.list <- c("scale_social", "scale_tech_in_field")
 analysis.scales <- analysis.scales %>% rowwise() %>% 
   dplyr::mutate(scale_social_tech = sum(c_across(any_of(scale.social.tech.list)))) %>%
   select(-all_of(scale.social.tech.list))
 
+# Bring the data back into a dataframe and return the key manuscriptIDs to row names
 analysis.scales <- as.data.frame(analysis.scales)
 rownames(analysis.scales) <- analysis.scales$manuscriptID
 analysis.scales$manuscriptID <- NULL
+
+# Scale the values so they are continuous values and normalized across the dataframe
 analysis.scales <- scale(analysis.scales)
 
-library(hrbrthemes)
-
-iu.colors <- c("#990000", "#FFAA00", "#056E41", "#006298", "#59264D")
-
-# Compute hierarchical clustering on principal components
+# Rename columns to easier-to-read descriptions
 colnames(analysis.scales) <- c("instructional_design", "specific_technologies",
                                "attitudes_beliefs", "simulations_games",
                                "teacher_pipeline", "methods", "phenomonology",
                                "subject_areas", "socializing_technology_use")
 
+############################################################
+#  Compute hierarchical clustering on principal components #
+############################################################
+
+# Re-run PCA on newly compiled and scaled dataframe
 analysis.pca <- PCA(analysis.scales, graph = FALSE)
+
+# Calculate HCPC
 res.hcpc <- HCPC(analysis.pca, graph = FALSE)
 
+# Create and output dendrogram of clusters by manuscriptID
 fviz_dend(res.hcpc,
           cex = 0.7,                     # Label size
           palette = iu.colors,               # Color palette see ?ggpubr::ggpar
